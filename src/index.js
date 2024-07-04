caf5b0c6ef804a80b611d18b367268eafa8c4527
Syifaul M <syifa.ulm01@gmail.com>
2024-07-04 11:16:08 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const port = 5000;
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: ["http://localhost:3000", "https://order-app-frontend.vercel.app"],
+        methods: ["GET", "POST"]
+    }
+});
+
+try {
+    app.use(cors());
+    app.use(express.json());
+
+    io.on('connection', (socket) => {
+        console.log(`New client connected: ${socket.id}`);
+
+        socket.on('joinRoom', (room) => {
+            socket.join(room);
+            console.log(`Perangkat ${socket.id} masuk ke room ${room}`);
+        });
+
+        socket.on('notify', (data) => {
+            const { target, content } = data;
+            if (target === 'semua-perangkat') {
+                console.log(target);
+                io.emit('notification', { content });
+            } else {
+                console.log(target);
+                io.to(target).emit('notification', { content });
+            }
+        });
+
+        socket.on('disconnect', () => {
+            console.log(`Client disconnected: ${socket.id}`);
+        });
+    });
+} catch (error) {
+    console.error(error);
+}
+
+httpServer.listen(5000, () => {
+    console.log(`Express server listening on port: ${port}...`);
+});
+
+export { io };
